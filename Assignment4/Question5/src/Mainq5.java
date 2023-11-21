91c00a02e14d140c18f8c16cc2c869065e060275
tushar <sunbeamtushar@gmail.com>
2023-11-21 15:19:39 +0530

    q5

---
+++ Assignment4/Question5/src/Mainq5.java
@@ -0,0 +1,17 @@
+
+public class Mainq5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        //5. Write a function which will insert values in sorted order into linked list.
+		LinkedList<Integer> list = new LinkedList<>();
+		
+		list.add(10);
+		list.add(20);
+		list.add(30);
+		list.add(40);
+		list.add(25);
+		list.Display();
+	}
+
+}
